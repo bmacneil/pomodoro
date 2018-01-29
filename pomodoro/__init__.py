7685371b4d4c07e140445941e2783e6385953402
Brad <brad.macneil5@gmail.com>
2018-01-29 09:46:54 -0800

    Added to gitigore. Added pomodoro session setting to the goal form

--- pomodoro/__init__.py
+++ pomodoro/__init__.py
@@ -3,70 +3,86 @@
 from tkinter import *
 
 
-class Form(object):
+class GoalForm(object):
     """docstring for ClassName"""
 
     def __init__(self):
-        self.company = ''
-        self.jobTitle = ''
-        self.location = ''
-        self.address = ''
+        self.project = ''
+        self.challenge = ['', '']
+        self.steps = ['', '', '']
+        self.sessions = 1
+        self.time = 25
+        self.shortBreak = 5
+        self.longBreak = 20
         self.makeform()
 
     def fetch(self, entries, root):
-        print(type(root))
-        print(type(entries))
+        # print(type(root))
+        # print(type(entries))
         for entry in entries:
-            if entry[0] == 'Company Name':
-                print("\n**************************")
-                self.company = entry[1].get()
-                print(self.company)
-            elif entry[0] == 'Job Title':
-                self.jobTitle = entry[1].get()
-                print(self.jobTitle)
-            elif entry[0] == 'Location':
-                self.location = entry[1].get()
-                print(self.location)
-            elif entry[0] == 'Address (st #, City, Prov PC)':
-                add = entry[1].get().split(',', 1)
-                self.address = [a.lstrip() for a in add]
-                print(self.address)
-            else:
-                print("\nFORM FETCH ERROR\n")
+            if entry[0] == 'Project':
+                self.project = entry[1].get()
+            elif entry[0] == '\ta)':
+                self.challenge[0] = entry[1].get()
+            elif entry[0] == '\tb)':
+                self.challenge[1] = entry[1].get()
+            elif entry[0] == '\t1.':
+                self.steps[0] = entry[1].get()
+            elif entry[0] == '\t2.':
+                self.steps[1] = entry[1].get()
+            elif entry[0] == '\t3.':
+                self.steps[2] = entry[1].get()
+            elif entry[0] == 'Sessions':
+                self.sessions = entry[1].get()
+            elif entry[0] == 'Time':
+                self.time = entry[1].get()
+            elif entry[0] == 'Short Break':
+                self.shortBreak = entry[1].get()
+            elif entry[0] == 'Long Break':
+                self.longBreak = entry[1].get()
+            # else:
+            #     print("\nFORM FETCH ERROR\n")
         root.destroy()
 
-    def openLink(self):
-        if self.link == "LINK":
-            print("No Link Available")
-        else:
-            # Open a new webpage for each link
-            print(self.link)
-            # Add escape characters to the link to avoid issues with cmd line
-            safelink = self.link.replace('&', '\&').replace('(', '\(').replace(')', '\)')
-            subprocess.call("google-chrome --incognito {0}".format(safelink), shell=True)
-            # subprocess.call("netsurf {0}".format(safelink), shell=True)
-            # webbrowser.open(self.link, new=2, autoraise=True)
+    def printEntries(self):
+        print("\n**************************")
+        print(self.project)
+        print(self.challenge[0])
+        print(self.challenge[1])
+        print(self.steps[0])
+        print(self.steps[1])
+        print(self.steps[2])
+        print(self.sessions)
+        print(self.time)
+        print(self.shortBreak)
+        print(self.longBreak)
+        print("**************************\n")
 
-    def removeJob(self, root):
-        self.company = "REMOVED"
-        self.jobTitle = "REMOVED"
-        self.location = "REMOVED"
-        self.address = ["REMOVED", "REMOVED"]
-        self.link = "REMOVED"
-        self.path = "REMOVED"
-        print("Job Removed")
-        root.destroy()
+    # def removeJob(self, root):
+    #     self.company = "REMOVED"
+    #     self.jobTitle = "REMOVED"
+    #     self.location = "REMOVED"
+    #     self.address = ["REMOVED", "REMOVED"]
+    #     self.link = "REMOVED"
+    #     self.path = "REMOVED"
+    #     print("Job Removed")
+    #     root.destroy()
 
     def makeform(self):
-        fields = ['Project', 'a)', 'b)', '1.', '2.', '3.']
-        text = [self.project, self.challenge[0], self.challenge[1], self.steps[0], self.steps[1], self.steps[2], self.address]
+        fields = ['Project', 'Worthy challenge:', '\ta)', '\tb)',
+                  'Steps to complete:', '\t1.', '\t2.', '\t3.', 'Sessions',
+                  'Time', 'Short Break', 'Long Breaks']
+        text = [self.project, None, self.challenge[0], self.challenge[1],
+                None, self.steps[0], self.steps[1], self.steps[2],
+                self.sessions, self.time, self.shortBreak, self.longBreak]
         root = Tk()
         entries = []
         for f, t in zip(fields, text):
             row = Frame(root)
-            lab = Label(row, width=25, text=f, anchor='w')
-            ent = Entry(row)
-            ent.insert(END, t)
+            lab = Label(row, width=15, text=f, anchor='w')
+            if t is not None:
+                ent = Entry(row, width=45)
+                ent.insert(END, t)
             row.pack(side=TOP, fill=X, padx=5, pady=5)
             lab.pack(side=LEFT)
             ent.pack(side=RIGHT, expand=YES, fill=X)
@@ -74,10 +90,7 @@ class Form(object):
 
         ents = entries
         ents[0][1].focus_set()
-        root.bind('<Return>', (lambda evnet, e=ents: self.fetch(e, root)))
-        # Button to open posting link
-        b2 = Button(root, text='Link', command=self.openLink)
-        b2.pack(side=LEFT, padx=5, pady=5)
+        root.bind('<Return>', (lambda event, e=ents: self.fetch(e, root)))
         # Button to close window
         b3 = Button(root, text='Quit', command=root.quit)
         b3.pack(side=LEFT, padx=5, pady=5)
@@ -85,11 +98,96 @@ class Form(object):
         b1 = Button(root, text='Enter', command=(
             lambda e=ents: self.fetch(e, root)))
         b1.pack(side=RIGHT, padx=5, pady=5)
-        # Button to remove jobs
-        rmvjob = Button(
-            root, text='Remove', command=lambda r=root: self.removeJob(root))
-        rmvjob.pack(side=LEFT, padx=5, pady=5)
+        # # Button to remove jobs
+        # rmvjob = Button(
+        #     root, text='Remove', command=lambda r=root: self.removeJob(root))
+        # rmvjob.pack(side=LEFT, padx=5, pady=5)
         root.mainloop()
 
 
-f = Form()
+class SettingsForm(object):
+    """docstring for ClassName"""
+
+    def __init__(self):
+        self.workTime = 25
+        self.breakTime = 5
+        self.workSessions = 1
+        self.makeform()
+
+    def fetch(self, entries, root):
+        # print(type(root))
+        # print(type(entries))
+        for entry in entries:
+            if entry[0] == 'Project':
+                self.project = entry[1].get()
+            elif entry[0] == '\ta)':
+                self.challenge[0] = entry[1].get()
+            elif entry[0] == '\tb)':
+                self.challenge[1] = entry[1].get()
+            elif entry[0] == '\t1.':
+                self.steps[0] = entry[1].get()
+            elif entry[0] == '\t2.':
+                self.steps[1] = entry[1].get()
+            elif entry[0] == '\t3.':
+                self.steps[2] = entry[1].get()
+            # else:
+            #     print("\nFORM FETCH ERROR\n")
+        root.destroy()
+
+    def printEntries(self):
+        print("\n**************************")
+        print(self.project)
+        print(self.challenge[0])
+        print(self.challenge[1])
+        print(self.steps[0])
+        print(self.steps[1])
+        print(self.steps[2])
+        print("**************************\n")
+
+    # def removeJob(self, root):
+    #     self.company = "REMOVED"
+    #     self.jobTitle = "REMOVED"
+    #     self.location = "REMOVED"
+    #     self.address = ["REMOVED", "REMOVED"]
+    #     self.link = "REMOVED"
+    #     self.path = "REMOVED"
+    #     print("Job Removed")
+    #     root.destroy()
+
+    def makeform(self):
+        fields = ['Project', 'Worthy challenge:', '\ta)',
+                  '\tb)', 'Steps to complete:', '\t1.', '\t2.', '\t3.']
+        text = [self.project, None, self.challenge[0], self.challenge[1],
+                None, self.steps[0], self.steps[1], self.steps[2]]
+        root = Tk()
+        entries = []
+        for f, t in zip(fields, text):
+            row = Frame(root)
+            lab = Label(row, width=15, text=f, anchor='w')
+            if t is not None:
+                ent = Entry(row, width=45)
+                ent.insert(END, t)
+            row.pack(side=TOP, fill=X, padx=5, pady=5)
+            lab.pack(side=LEFT)
+            ent.pack(side=RIGHT, expand=YES, fill=X)
+            entries.append((f, ent))
+
+        ents = entries
+        ents[0][1].focus_set()
+        root.bind('<Return>', (lambda event, e=ents: self.fetch(e, root)))
+        # Button to close window
+        b3 = Button(root, text='Quit', command=root.quit)
+        b3.pack(side=LEFT, padx=5, pady=5)
+        # Button to enter info. Same as return key
+        b1 = Button(root, text='Enter', command=(
+            lambda e=ents: self.fetch(e, root)))
+        b1.pack(side=RIGHT, padx=5, pady=5)
+        # # Button to remove jobs
+        # rmvjob = Button(
+        #     root, text='Remove', command=lambda r=root: self.removeJob(root))
+        # rmvjob.pack(side=LEFT, padx=5, pady=5)
+        root.mainloop()
+
+
+f = GoalForm()
+print(f.printEntries())
