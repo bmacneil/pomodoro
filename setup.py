86b2fc81ea424574bbb486289268fb2abbffe67c
Brad <brad.macneil5@gmail.com>
2018-02-01 16:06:18 -0800

    Added tinydb and ujson to the project

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ try:
 except ImportError:
     from distutils.core import setup
 config = {
-    'description': 'My Project',
+    'description': 'A pomodoro program with a simple gui to track work sessions.',
     'author': 'Brad MacNeil',
     'url': 'URL to get it',
     'download_url': 'URL to download it',
     'author_email': 'My Email',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['nose', 'pyyaml', 'schedule', 'tinydb', 'ujson'],
     'packages': ['pomodoro'],
     'scripts': [],
     'name': 'pomodoro'
